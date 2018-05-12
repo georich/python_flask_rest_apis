07f935ff0bb4eef09941613a1bbe7629580a0fdf
George Richards <georich@protonmail.com>
2018-05-12 23:09:10 +0100

    Getting all items from database

--- sql_storage/item.py
+++ sql_storage/item.py
@@ -110,4 +110,19 @@ class ItemList(Resource):
 
     def get(self):
         """GET /items route."""
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+
+        for row in result:
+            items.append({
+                'name': row[0],
+                'price': row[1]
+            })
+
+        connection.close()
+
         return {'items': items}
