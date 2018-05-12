a695f614ef116d18646682ce3d3ad1076d3abf55
George Richards <georich@protonmail.com>
2018-05-12 09:47:13 +0100

    Add docstrings to satisfy pydocstyle

--- sql_storage/app.py
+++ sql_storage/app.py
@@ -1,3 +1,4 @@
+"""Flask app for simple store of items."""
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
@@ -14,6 +15,8 @@ items = []
 
 
 class Item(Resource):
+    """Resource for item endpoints."""
+
     parser = reqparse.RequestParser()
     parser.add_argument(
         'price',
@@ -23,10 +26,12 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        """GET /item route."""
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        """POST /item route."""
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {
                 'message': f"An item with name '{name}' already exists."
@@ -37,11 +42,13 @@ class Item(Resource):
         return item, 201
 
     def delete(self, name):
+        """DELETE /item route."""
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
     def put(self, name):
+        """PUT /item route."""
         data = Item.parser.parse_args()
 
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -54,7 +61,10 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    """Resource for items endpoint."""
+
     def get(self):
+        """GET /items route."""
         return {'items': items}
 
 
