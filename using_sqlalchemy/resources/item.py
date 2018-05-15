31cc1262c99ae8f38d31af5f146477b4f4a4fe86
George Richards <georich@protonmail.com>
2018-05-15 16:37:32 +0100

    ItemList get method changed to use SQLAlchemy

--- using_sqlalchemy/resources/item.py
+++ using_sqlalchemy/resources/item.py
@@ -1,5 +1,4 @@
 """Set up of resources for app."""
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -70,19 +69,4 @@ class ItemList(Resource):
 
     def get(self):
         """GET /items route."""
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-
-        for row in result:
-            items.append({
-                'name': row[0],
-                'price': row[1]
-            })
-
-        connection.close()
-
-        return {'items': items}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
