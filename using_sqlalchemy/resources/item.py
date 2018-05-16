9ea72c7151416bb14232263619ca1dc8dd740391
George Richards <georich@protonmail.com>
2018-05-16 11:34:19 +0100

    Add a store model and link it to Item

--- using_sqlalchemy/resources/item.py
+++ using_sqlalchemy/resources/item.py
@@ -12,7 +12,15 @@ class Item(Resource):
         'price',
         type=float,
         required=True,
-        help="This field cannot be left blank!")
+        help="This field cannot be left blank!"
+    )
+
+    parser.add_argument(
+        'store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
+    )
 
     @jwt_required()
     def get(self, name):
@@ -31,7 +39,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
 
         try:
             item.save_to_db()
@@ -55,7 +63,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, data['price'], data['store_id'])
         else:
             item.price = data['price']
 
